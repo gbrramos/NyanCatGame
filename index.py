07f10fe3e863a007cb8ab126e3fff4c70d85880c
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-04-02 10:34:49 -0300

    add musica

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import pygame
 from pygame import font
+
 pygame.init()
+pygame.font.init()                              
 
 window = pygame.display.set_mode((800,600))
-pygame.display.set_caption("AAAAAAAAAAAAAAAAAAA")
+pygame.display.set_caption("Nyan Cat's New Adventure")
 
 x = 328
 y = 276
@@ -13,6 +15,11 @@ back = pygame.image.load('img/bg.jpg')
 nyan = pygame.image.load('img/nyan.gif')
 back_size = pygame.transform.scale(back,(800,600))
 nyan_small = pygame.transform.scale(nyan,(150,100))
+
+music = pygame.mixer.music.load('musics/bg-sound-nyan-trap-remix.mp3')
+pygame.mixer.music.set_volume(0.07)
+pygame.mixer.music.get_volume()
+pygame.mixer.music.play(10)
 
 open_window = True
 while open_window:
@@ -35,13 +42,16 @@ while open_window:
         x = x - vel    
     window.blit(back_size,(0,0))
     window.blit(nyan_small,(x,y))
-   
-    # pygame.font.init()                              
-    # fontesys1 = pygame.font.SysFont(x, 60)      
-    # fontesys2 = pygame.font.SysFont(y, 60)                
-    # txttela = fontesys.render(txt, 1, (255,255,255))  
-    # window.blit(txttela,(10,10))   
-    # window.blit(txttela,(10,10))                      
-    # pygame.font.get_default_font()
+
+    strX = 'X: '+str(x)
+    strY = 'Y: '+str(y)
+
+    fontX = pygame.font.SysFont(strX, 30)      
+    fontY = pygame.font.SysFont(strY, 30)                
+    txtX = fontX.render(strX, 1, (255,255,255))  
+    txtY = fontY.render(strY, 1, (255,255,255))  
+    window.blit(txtX,(10,10))   
+    window.blit(txtY,(10,40))    
+    pygame.font.get_default_font()
     pygame.display.flip()
 pygame.quit()
